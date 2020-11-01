23bcc4671c0a29b97d71434246388a3f95245257
kewin <404502753@qq.com>
2020-11-01 17:13:23 +0800

    article字段修改

--- server/libs/db/src/models/article.model.ts
+++ server/libs/db/src/models/article.model.ts
@@ -3,13 +3,17 @@ import { modelOptions, prop } from "@typegoose/typegoose";
 
 @modelOptions({
   schemaOptions: {
-    timestamps: true
+    // timestamps: true
   }
 })
 export class Article {
   @prop()
   @ApiProperty({ description: '标题', example: 'title' })
   title: string;
+
+  @prop()
+  @ApiProperty({ description: '标题', example: 'title' })
+  type: string;
 
   @prop()
   @ApiProperty({ description: '文章内容', example: 'content' })
@@ -26,4 +30,12 @@ export class Article {
   @prop()
   @ApiProperty({ description: '点赞数量', example: '1000' })
   star: number
+
+  @prop()
+  @ApiProperty({ description: '发布时间', example: '2020-02-20' })
+  releaseTime: number
+
+  @prop()
+  @ApiProperty({ description: '更新时间', example: '2020-02-20' })
+  updateTime: number
 }
